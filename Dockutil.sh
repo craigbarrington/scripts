6e58925f18d3af0031d9d98a8bc731457d6c663d
Craig Barrington <craig.barrington@outlook.com>
2022-10-12 14:54:31 +0100

    Update and rename dockUtil.sh to Dockutil.sh

--- Dockutil.sh
+++ Dockutil.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##################################
-###   Docutil - Default Dock   ###
+###   Dockutil - Default Dock   ###
 ##################################
 
 # https://github.com/kcrawford/dockutil
