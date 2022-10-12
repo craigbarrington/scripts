5a3a5aa9073fd025c8d105438399a3d128a45e09
Craig Barrington <craig.barrington@outlook.com>
2022-10-12 14:53:41 +0100

    Create dockUtil.sh

---
+++ dockUtil.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################################
+###   Docutil - Default Dock   ###
+##################################
+
+# https://github.com/kcrawford/dockutil
+
+/usr/local/bin/dockutil --remove all
+
+/usr/local/bin/dockutil --add '/Applications/Google Chrome.app' --allhomes
+/usr/local/bin/dockutil --add '/Applications/Microsoft Word.app' --allhomes
+/usr/local/bin/dockutil --add '/Applications/Microsoft Excel.app' --allhomes
+/usr/local/bin/dockutil --add '/Applications/Microsoft PowerPoint.app' --allhomes
+/usr/local/bin/dockutil --add '~/Downloads' --view fan --display folder --allhomes
+
+exit 0
